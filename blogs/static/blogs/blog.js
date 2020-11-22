b36bfe1bda0c582a36e54d25da15b3d889382a24
AjayRangarajan <ajayrangarajan5@gmail.com>
2020-11-22 17:12:40 +0530

    Add more style for the website

--- blogs/static/blogs/blog.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded',()=>{
-    var blog_creation_form = document.querySelector('#blog-creation-form').innerHTML
-    console.log(blog_creation_form)
-})
---
+++ home/static/home/home.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    document.querySelector('.logout-button').onclick=()=>{
+        alert('Are you sure wants to logout?')
+    }
+})
