ed8aa735a24c32353c1089a657758da5c3b7694b
AjayRangarajan <ajayrangarajan5@gmail.com>
2020-11-23 15:59:58 +0530

    Ready for production

--- authors/models.py
+++ authors/models.py
@@ -3,8 +3,8 @@ from cloudinary.models import CloudinaryField
 from django.contrib.auth.models import User
 class Authors(models.Model):
     default=models.OneToOneField(User,on_delete=models.CASCADE,related_name='author')
-    profile_pic=models.ImageField(upload_to="author_profiles",blank=True,null=True)
-    # profile_pic=CloudinaryField('image')
+    # profile_pic=models.ImageField(upload_to="author_profiles",blank=True,null=True)
+    profile_pic=CloudinaryField('image')
     profile_image_link=models.URLField(blank=True,null=True)
     alt_msg=models.CharField(max_length=100,default="Author's Profile picture")
 
