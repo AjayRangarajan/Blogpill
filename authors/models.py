e7dd7e85367b315ed54b2a48dbb62e77c8d9f181
AjayRangarajan <ajayrangarajan5@gmail.com>
2021-09-19 16:58:52 +0530

    Remove cloudinary field for images and add ImageField

--- authors/models.py
+++ authors/models.py
@@ -5,8 +5,8 @@ from cloudinary.models import CloudinaryField
 
 class Profile(models.Model):
     user = models.OneToOneField(User, related_name='profile', on_delete=models.CASCADE)
-    profile_pic=CloudinaryField('image')
-    # profile_pic = models.ImageField(upload_to='profile_pics', blank=True,null=True)
+    # profile_pic=CloudinaryField('image')
+    profile_pic = models.ImageField(upload_to='profile_pics', blank=True,null=True)
     # profile_pic = models.ImageField(upload_to='profile_pics',default='profile_pics/default_profile_pic.png',blank=True,null=True)
 
     def __str__(self):
