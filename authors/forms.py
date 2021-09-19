69ff1b6d3815b79ed5c7421420d8360674f4d6df
AjayRangarajan <ajayrangarajan5@gmail.com>
2021-09-19 16:13:01 +0530

    Commit untracked files

---
+++ authors/forms.py
@@ -0,0 +1,29 @@
+from django.contrib.auth import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+from django.db.models import fields
+from .models import Profile
+
+class UserRegistrationForm(UserCreationForm):
+    first_name = forms.CharField(max_length=100)
+    last_name = forms.CharField(max_length=100)
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username','first_name','last_name','email','password1','password2']
+
+class UserUpdateForm(forms.ModelForm):
+    first_name = forms.CharField(max_length=100)
+    last_name = forms.CharField(max_length=100)
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username','first_name','last_name','email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['profile_pic']
